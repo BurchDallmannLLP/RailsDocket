439d0b481fede734a25b41179d4d9c3d384d7117
jerseycoder <michelle@codeforamerica.org>
2012-01-16 18:53:20 -0500

    Basic Rails install with homepage

---
+++ RailsDocket/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsDocket::Application.config.secret_token = 'a960bc071ccee9751c93b170a3dfc231afd7bcf5fe88a27bf92ffe51e49d26763b540f782f6fb88203177e9f3ba82e10fcb7ca45e4251b174d365382e8e8119c'
